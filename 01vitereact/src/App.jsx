042c0d58553dfc6664f19a6c5ac8ada94201128f
Rathindhar <rathindhar2004@gmail.com>
2026-02-11 22:12:54 +0530

    Learning react and using Vite to create react application

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "Chai aur COde"
+  return (
+    <>
+    <h2> Hello Brother</h2>
+    <Chai/>
+    <h5>This is the bottom one brother {username}</h5>
+    </>
+    
+  )
+}
+
+export default App
