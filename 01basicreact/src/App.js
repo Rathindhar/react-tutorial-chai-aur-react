8daa1851f8f555408e71c4d18328808014361356
Rathindhar <rathindhar2004@gmail.com>
2026-02-11 22:06:48 +0530

    01 Basic react using create react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+    <h1>Hello Brother</h1>
+    <Chai/>
+    <h4>This is bottom brother</h4>
+    </>
+  );
+}
+
+export default App;
